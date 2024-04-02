4aad9d817ef887e079ea41c99f37b3df61d3d71f
dewi sekar <nanamint623@gmail.com>
2024-04-02 19:42:39 +0700

    add fiture searching and pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,14 +4,26 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post ;
+use App\Models\User ;
+use App\Models\Category ;
 class PostController extends Controller
 {
     public function index(){
-        return view('posts', [ "title" => "All Posts",
-        // "posts" => Post::all()
-        "posts" => Post::latest()->get()
-      ]);
+        $title = '';
+        if(request('category')){
+            $category = Category::firstWhere('slug' , request('category'));
+            $title = 'in ' . $category->name ;
+        }
+        if(request('author')){
+            $author = User::firstWhere('username' , request('author'));
+            $title = 'by ' . $author->username ;
+        }
+        return view('posts', [ 
+            "title" => "All Posts  $title",
+            "posts" =>  Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString()
+        ]);
     }
+    
 
     public function show(Post $post){
         return view('post', [
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,7 +9,7 @@
         <img src="https:/source.unsplash.com/1200x400?{{ $post->category->name}}" class="card-img-top img-fluid" alt="{{ $post->category->name}}">
 
         <h1>{{ $post->title }}</h1>
-<h5>By <a href="/authors/{{$post->author->username}}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug}}">{{ $post->category->name }}</a></h5>
+<h5>By <a href="/blog?author={{$post->author->username}}">{{ $post->author->name }}</a> in <a href="/blog?category={{ $post->category->slug}}">{{ $post->category->name }}</a></h5>
 <article class="fs-5 my-3" >
 {!! $post->body !!}
 </article>
